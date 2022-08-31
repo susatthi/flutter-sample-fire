77232d3b3cc6043c00be4359c7bc4ea367b065d0
susa.keyber <susa@keyber.jp>
2022-09-01 05:50:25 +0900

    🚀 feat:  Firestoreを使ったカウンターアプリを実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: avoid_print
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -14,10 +15,27 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  // 匿名ユーザーでサインインする
-  // サインイン済みなら何もしないので無条件に呼び出してOK
-  final credential = await FirebaseAuth.instance.signInAnonymously();
-  print('uid = ${credential.user?.uid}');
+  // 匿名ユーザーを取得する
+  final firebaseUser = await FirebaseAuth.instance.userChanges().first;
+  print('uid = ${firebaseUser?.uid}');
+  if (firebaseUser == null) {
+    // 未サインインなので匿名ユーザーでサインインする
+    // サインイン済みなら何もしないので無条件に呼び出してOK
+    final credential = await FirebaseAuth.instance.signInAnonymously();
+    final uid = credential.user!.uid;
+    print('Signed in: uid = $uid');
+
+    // userドキュメントを作成する
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(uid)
+        .set(<String, dynamic>{
+      'counter': 0,
+      'createdAt': FieldValue.serverTimestamp(),
+      'updatedAt': FieldValue.serverTimestamp(),
+    });
+    print('Created user document');
+  }
 
   runApp(const MyApp());
 }
@@ -37,29 +55,31 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  String get _uid => FirebaseAuth.instance.currentUser!.uid;
+  DocumentReference<Map<String, dynamic>> get _userRef =>
+      FirebaseFirestore.instance.collection('users').doc(_uid);
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  Future<void> _incrementCounter() async {
+    await _userRef.update(<String, dynamic>{
+      'counter': FieldValue.increment(1),
+      'updatedAt': FieldValue.serverTimestamp(),
     });
+    print('Updated user document');
   }
+
+  Stream<DocumentSnapshot<Map<String, dynamic>>>
+      _changesUserDocumentSnapshot() => _userRef.snapshots();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
@@ -68,9 +88,28 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+              stream: _changesUserDocumentSnapshot(),
+              builder: (context, snapshot) {
+                print(
+                  'Change user doc: '
+                  'connectionState = ${snapshot.connectionState}',
+                );
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                }
+
+                final data = snapshot.data?.data();
+                if (data == null) {
+                  return const CircularProgressIndicator();
+                }
+
+                final count = data['counter'] as int;
+                return Text(
+                  '$count',
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
             ),
           ],
         ),
